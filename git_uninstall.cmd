c474a732da19fab308fd41344c71c30324b83a9e
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-10-16 13:17:44 +0100

    Create git_uninstall.cmd

---
+++ git_uninstall.cmd
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "possibleInstalledPaths=C:\Program Files\Git C:\Program Files (x86)\Git C:\git"
+set gitFound=false
+
+for %%G in (%possibleInstalledPaths%) do (
+    if exist "%%G" (
+        taskkill /F /IM bash.exe /T >nul 2>&1
+        taskkill /F /IM putty* /T >nul 2>&1
+        
+        echo Removing Git from "%%G"
+
+        for %%U in ("%%G\unins*.exe") do (
+            "%%U" /SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS
+        )
+
+        set gitFound=true
+    )
+)
+
+if "!gitFound!"=="false" (
+    echo No Git installation found. Nothing to uninstall.
+)
